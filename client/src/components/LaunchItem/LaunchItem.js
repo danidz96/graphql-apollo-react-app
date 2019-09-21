f2490c039a0ba2b5e97ef89664b54d9f77c72343
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-21 16:43:03 +0200

    add launch item

--- client/src/components/LaunchItem/LaunchItem.js
+++ client/src/components/LaunchItem/LaunchItem.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Card, Col, Button } from 'antd';
 
 const LaunchItem = ({ launch: { flight_number, mission_name, launch_date_local, launch_success } }) => {
 	return (
-		<div>
-			<p>{flight_number}</p>
-			<p>{mission_name}</p>
-			<p>{launch_date_local}</p>
-			<p>{launch_success}</p>
-		</div>
+		<Card
+			style={{ width: '100%', marginBottom: '20px' }}
+			cover={
+				<img
+					alt="example"
+					style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+					src="https://img.youtube.com/vi/0a_00nJ_Y88/0.jpg"
+				/>
+			}
+		>
+			<Col span={20}>
+				<h1>Mission: {mission_name}</h1>
+				<p>{launch_date_local}</p>
+				<p>{launch_success}</p>
+			</Col>
+			<Col span={4}>
+				<Button>Launch Details</Button>
+			</Col>
+		</Card>
 	);
 };
 
