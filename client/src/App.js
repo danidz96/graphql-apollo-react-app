d71c290c9bd98657356c6895586f6d421c2233ef
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-21 13:30:08 +0200

    set up react apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost/';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
 
-function App() {
+const client = new ApolloClient({
+	uri: 'https://localhost:5000/graphql'
+});
+
+const App = () => {
 	return (
-		<div className="App">
-			<h1>SpaceX</h1>
-		</div>
+		<ApolloProvider client={client}>
+			<div className="App">
+				<h1>SpaceX</h1>
+			</div>
+		</ApolloProvider>
 	);
-}
+};
 
 export default App;
