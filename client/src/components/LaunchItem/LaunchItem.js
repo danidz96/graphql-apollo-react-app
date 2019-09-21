de64005216634d3914d3cf4c62408e0e2cb005f6
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-21 16:54:43 +0200

    remove image

--- client/src/components/LaunchItem/LaunchItem.js
+++ client/src/components/LaunchItem/LaunchItem.js
@@ -3,16 +3,7 @@ import { Card, Col, Button } from 'antd';
 
 const LaunchItem = ({ launch: { flight_number, mission_name, launch_date_local, launch_success } }) => {
 	return (
-		<Card
-			style={{ width: '100%', marginBottom: '20px' }}
-			cover={
-				<img
-					alt="example"
-					style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-					src="https://img.youtube.com/vi/0a_00nJ_Y88/0.jpg"
-				/>
-			}
-		>
+		<Card style={{ width: '100%', marginBottom: '20px' }}>
 			<Col span={20}>
 				<h1>Mission: {mission_name}</h1>
 				<p>{launch_date_local}</p>
