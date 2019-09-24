acc68522eba2e7b9697affa56f42ce2b87f18a61
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-24 16:26:40 +0200

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost/';
 import { ApolloProvider } from '@apollo/react-hooks';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches/Launches';
+import Launch from './components/Launch/Launch';
+import './App.css';
 
 const client = new ApolloClient({
 	uri: 'http://localhost:5000/graphql'
@@ -11,10 +13,13 @@ const client = new ApolloClient({
 const App = () => {
 	return (
 		<ApolloProvider client={client}>
-			<div className="App">
-				<h1>SpaceX</h1>
-				<Launches />
-			</div>
+			<Router>
+				<div className="App">
+					<h1>SpaceX</h1>
+					<Route exact path="/" component={Launches} />
+					<Route exact path="/launch/:flight:number" component={Launch} />
+				</div>
+			</Router>
 		</ApolloProvider>
 	);
 };
