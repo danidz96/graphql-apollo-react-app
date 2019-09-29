f2e021cc36fb7c0d1c60a72f65d849fa9f05a132
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-29 20:51:48 +0200

    show launch details

--- client/src/components/Launch/Launch.js
+++ client/src/components/Launch/Launch.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import { Link } from 'react-router-dom';
+import { Descriptions, Button } from 'antd';
+import Moment from 'react-moment';
 
 const GET_LAUNCH = gql`
 	query GetLaunch($flight_number: Int!) {
@@ -24,9 +26,37 @@ const Launch = (props) => {
 	let { flight_number } = props.match.params;
 	flight_number = parseInt(flight_number);
 	const { loading, error, data } = useQuery(GET_LAUNCH, { variables: { flight_number } });
+	return (
+		<React.Fragment>
+			{loading && <p>Loading...</p>}
 
-	console.log(data);
-	return <React.Fragment>Launch</React.Fragment>;
+			{!loading && (
+				<React.Fragment>
+					<h1>
+						Mission: <span>{data.launch.mission_name}</span>
+					</h1>
+					<h2>Launch Details</h2>
+					<Descriptions>
+						<Descriptions.Item label="Flight Number">{data.launch.flight_number}</Descriptions.Item>
+						<Descriptions.Item label="Launch Year">{data.launch.launch_year}</Descriptions.Item>
+						<Descriptions.Item label="Launch Success">{data.launch.launch_success}</Descriptions.Item>
+						<Descriptions.Item label="Launch Date Local">
+							<Moment format="DD-MM-YYYY HH:mm">{data.launch.launch_date_local}</Moment>
+						</Descriptions.Item>
+					</Descriptions>
+					<h2>Rocket Details</h2>
+					<Descriptions>
+						<Descriptions.Item label="Rocket ID">{data.launch.rocket.rocket_id}</Descriptions.Item>
+						<Descriptions.Item label="Rocket Name">{data.launch.rocket.rocket_name}</Descriptions.Item>
+						<Descriptions.Item label="Rocket Type">{data.launch.rocket.rocket_type}</Descriptions.Item>
+					</Descriptions>
+					<Link to={`/`}>
+						<Button>Back</Button>
+					</Link>
+				</React.Fragment>
+			)}
+		</React.Fragment>
+	);
 };
 
 export default Launch;
