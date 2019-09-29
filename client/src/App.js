b58234636db994f569dc1a9c8a3bc8ab92adba99
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-29 21:15:45 +0200

    setup heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Launch from './components/Launch/Launch';
 import './App.css';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:5000/graphql'
+	uri: '/graphql'
 });
 
 const App = () => {
