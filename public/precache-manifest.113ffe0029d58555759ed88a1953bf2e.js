812bd7eb2a32992fbf41883479c90519a6bb7fa4
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-29 21:17:12 +0200

    setup heroku deploy

---
+++ public/precache-manifest.113ffe0029d58555759ed88a1953bf2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162fd32d94c52908996d51109159cc17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769fa2ace164c4424a32",
+    "url": "/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "036c6ac2d17b7d7aa989",
+    "url": "/static/css/main.d03622a4.chunk.css"
+  },
+  {
+    "revision": "769fa2ace164c4424a32",
+    "url": "/static/js/2.06f4bfc8.chunk.js"
+  },
+  {
+    "revision": "036c6ac2d17b7d7aa989",
+    "url": "/static/js/main.01ba5568.chunk.js"
+  },
+  {
+    "revision": "8a8edf166e5c49aab1ae",
+    "url": "/static/js/runtime~main.9ed2903b.js"
+  }
+]);
