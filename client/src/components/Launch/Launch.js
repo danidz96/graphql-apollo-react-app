3151d3858e5a902f5b9413e114181e1be6d74f06
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-07 00:17:56 +0200

    remove unused imports

--- client/src/components/Launch/Launch.js
+++ client/src/components/Launch/Launch.js
@@ -6,57 +6,57 @@ import { Descriptions, Button } from 'antd';
 import Moment from 'react-moment';
 
 const GET_LAUNCH = gql`
-	query GetLaunch($flight_number: Int!) {
-		launch(flight_number: $flight_number) {
-			flight_number
-			mission_name
-			launch_year
-			launch_success
-			launch_date_local
-			rocket {
-				rocket_id
-				rocket_name
-				rocket_type
-			}
-		}
-	}
+  query GetLaunch($flight_number: Int!) {
+    launch(flight_number: $flight_number) {
+      flight_number
+      mission_name
+      launch_year
+      launch_success
+      launch_date_local
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
 `;
 
 const Launch = (props) => {
-	let { flight_number } = props.match.params;
-	flight_number = parseInt(flight_number);
-	const { loading, error, data } = useQuery(GET_LAUNCH, { variables: { flight_number } });
-	return (
-		<React.Fragment>
-			{loading && <p>Loading...</p>}
+  let { flight_number } = props.match.params;
+  flight_number = parseInt(flight_number);
+  const { loading, data } = useQuery(GET_LAUNCH, { variables: { flight_number } });
+  return (
+    <React.Fragment>
+      {loading && <p>Loading...</p>}
 
-			{!loading && (
-				<React.Fragment>
-					<h1>
-						Mission: <span>{data.launch.mission_name}</span>
-					</h1>
-					<h2>Launch Details</h2>
-					<Descriptions>
-						<Descriptions.Item label="Flight Number">{data.launch.flight_number}</Descriptions.Item>
-						<Descriptions.Item label="Launch Year">{data.launch.launch_year}</Descriptions.Item>
-						<Descriptions.Item label="Launch Success">{data.launch.launch_success}</Descriptions.Item>
-						<Descriptions.Item label="Launch Date Local">
-							<Moment format="DD-MM-YYYY HH:mm">{data.launch.launch_date_local}</Moment>
-						</Descriptions.Item>
-					</Descriptions>
-					<h2>Rocket Details</h2>
-					<Descriptions>
-						<Descriptions.Item label="Rocket ID">{data.launch.rocket.rocket_id}</Descriptions.Item>
-						<Descriptions.Item label="Rocket Name">{data.launch.rocket.rocket_name}</Descriptions.Item>
-						<Descriptions.Item label="Rocket Type">{data.launch.rocket.rocket_type}</Descriptions.Item>
-					</Descriptions>
-					<Link to={`/`}>
-						<Button>Back</Button>
-					</Link>
-				</React.Fragment>
-			)}
-		</React.Fragment>
-	);
+      {!loading && (
+        <React.Fragment>
+          <h1>
+            Mission: <span>{data.launch.mission_name}</span>
+          </h1>
+          <h2>Launch Details</h2>
+          <Descriptions>
+            <Descriptions.Item label="Flight Number">{data.launch.flight_number}</Descriptions.Item>
+            <Descriptions.Item label="Launch Year">{data.launch.launch_year}</Descriptions.Item>
+            <Descriptions.Item label="Launch Success">{data.launch.launch_success}</Descriptions.Item>
+            <Descriptions.Item label="Launch Date Local">
+              <Moment format="DD-MM-YYYY HH:mm">{data.launch.launch_date_local}</Moment>
+            </Descriptions.Item>
+          </Descriptions>
+          <h2>Rocket Details</h2>
+          <Descriptions>
+            <Descriptions.Item label="Rocket ID">{data.launch.rocket.rocket_id}</Descriptions.Item>
+            <Descriptions.Item label="Rocket Name">{data.launch.rocket.rocket_name}</Descriptions.Item>
+            <Descriptions.Item label="Rocket Type">{data.launch.rocket.rocket_type}</Descriptions.Item>
+          </Descriptions>
+          <Link to={`/`}>
+            <Button>Back</Button>
+          </Link>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default Launch;
--- client/src/components/LaunchItem/LaunchItem.js
+++ client/src/components/LaunchItem/LaunchItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Button, Row, Tag, Typography } from 'antd';
+import { Card, Col, Button, Tag, Typography } from 'antd';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import './LaunchItem.css';
@@ -7,27 +7,27 @@ import './LaunchItem.css';
 const { Title } = Typography;
 
 const LaunchItem = ({ launch: { flight_number, mission_name, launch_date_local, launch_success } }) => {
-	return (
-		<Card className="card">
-			<Col span={20}>
-				<div className="missionData">
-					<Title level={2}>Mission: {mission_name}</Title>
-					<div className="statusBudget">
-						<Tag color={launch_success ? 'green' : 'red'}>{launch_success ? 'Success' : 'Fail'}</Tag>
-					</div>
-				</div>
-				<p>
-					<Moment format="DD-MM-YYYY HH:mm">{launch_date_local}</Moment>
-				</p>
-				<p>{launch_success}</p>
-			</Col>
-			<Col span={4}>
-				<Link to={`/launch/${flight_number}`}>
-					<Button>Launch Details</Button>
-				</Link>
-			</Col>
-		</Card>
-	);
+  return (
+    <Card className="card">
+      <Col span={20}>
+        <div className="missionData">
+          <Title level={2}>Mission: {mission_name}</Title>
+          <div className="statusBudget">
+            <Tag color={launch_success ? 'green' : 'red'}>{launch_success ? 'Success' : 'Fail'}</Tag>
+          </div>
+        </div>
+        <p>
+          <Moment format="DD-MM-YYYY HH:mm">{launch_date_local}</Moment>
+        </p>
+        <p>{launch_success}</p>
+      </Col>
+      <Col span={4}>
+        <Link to={`/launch/${flight_number}`}>
+          <Button>Launch Details</Button>
+        </Link>
+      </Col>
+    </Card>
+  );
 };
 
 export default LaunchItem;
